4947d950b51b53057200c7310648902382dc84a0
Michiel Burger <michielburger0@gmail.com>
2020-09-04 10:27:52 +1200

    Shooting functionality implemented
    Ray casting from centre screen completed

---
+++ 3rd Person Survival Shooter Pro/Assets/Scripts/Shoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Firing();
+    }
+
+    void Firing()
+    {
+        //left click fire
+        //cast ray from centre of screen
+        //debug the name of the object hit
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 centreViewPort = new Vector3(.5f, .5f, 0);
+            Ray rayOrigin = Camera.main.ViewportPointToRay(centreViewPort);
+            RaycastHit hitInfo;
+
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                if (hitInfo.collider != null)
+                {
+                    Debug.Log("Object hit: " + hitInfo.collider.name);
+                }
+            }
+        }
+    }
+}
